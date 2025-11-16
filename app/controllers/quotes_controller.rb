7130a442b62b5ece28b9446f3fea517b3134633e
ate-9 <adam.t.eldridge@gmail.com>
2025-11-17 00:21:55 +0900

    up til chapter 5

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -16,7 +16,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,18 +29,20 @@ class QuotesController < ApplicationController
   end
 
   def update
-    @quote = Quote.new(quote_params)
-
-    if @quote.save
+    if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was successfully edited."
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully deleted."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully deleted." }
+      format.turbo_stream
+    end
   end
 
   private
